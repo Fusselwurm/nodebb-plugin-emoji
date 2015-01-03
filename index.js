580c68685759f088124155aa41db3aae3676000e
Moritz Schmidt <fusselwurm@gmail.com>
2015-01-03 23:11:35 +0100

    0.6.0 compatibility

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ var	emoji = require('emoji-images'),
 	nconf = module.parent.require('nconf'),
 	Emoji = {};
 
-Emoji.addEmoji = function(postContent) {
+function render(postContent) {
 	return postContent.replace(/(^|<\/code>)([^<]*|<(?!code>))*(<code>|$)/g, function (match) {
 		return emoji(match, nconf.get('url') + '/plugins/nodebb-plugin-emoji/images');
 	});
+}
+
+Emoji.addEmoji = function(data, callback) {
+	data.postData.content = render(data.postData.content);
+	callback(null, data);
 };
 
 module.exports = Emoji;
